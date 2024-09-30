f5ba10db77a72c053ad8310bb99c211d7a37fb22
jassbir2024 <computertech147@gmail.com>
2024-10-01 01:07:01 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,22 @@
+const ages = [32, 33, 16, 40];
+
+function checkAdult(age) {
+  return age >= 18;
+}
+
+const adults = ages.filter(checkAdult);
+console.log("Adults:", adults);
+const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+const longWords = words.filter(function (word) {
+  return word.length > 6;
+});
+
+console.log("Long words:", longWords);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = numbers.filter(function (num) {
+  return num % 2 === 0;
+});
+
+console.log("Even numbers:", evenNumbers);
